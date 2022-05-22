2b9bf8eec1259ab69d092e2476d2ba2253a22f44
Collins <mutaic84@gmail.com>
2022-05-22 22:49:12 +0300

    update styling

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -6,32 +6,32 @@ function Thumbnails() {
   return (
     <div className='max-w-8xl px-5 mx-auto container grid gap-4 grid-cols-4 mt-6'>
       <Link href='/information'>
-        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
           <div>
             <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
           </div>
           <h4 className='px-4 py-3 text-lg'>captain</h4>
         </div>
       </Link>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
         <div>
           <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
         </div>
         <h4 className='px-4 py-3 text-lg'>captain</h4>
       </div>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
         <div>
           <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
         </div>
         <h4 className='px-4 py-3 text-lg'>captain</h4>
       </div>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
         <div>
           <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
         </div>
         <h4 className='px-4 py-3 text-lg'>captain</h4>
       </div>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
         <div>
           <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
         </div>
