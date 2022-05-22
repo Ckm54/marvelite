023355d943140a5467f7145da3157c93eb1dd830
Collins <mutaic84@gmail.com>
2022-05-22 22:48:56 +0300

    add styling to the searchbar component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Search() {
   return (
     <form>
-      <input type='text' />
-      <button>Search</button>
+      <input className="font-semibold text-lg text-white text-lg border-b-4 border-titleRed px-2 mr-3 focus:outline-none bg-transparent placeholder-gray-500" placeholder='Input superhero name' type='text' />
+      <button className='text-2xl bg-titleRed font-semibold text-white px-4 py-1 rounded hover:bg-red-600'>Search</button>
     </form>
   )
 }
