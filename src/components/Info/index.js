cd48d834002465686a40afe04d079e12ffdf1b6b
Collins <mutaic84@gmail.com>
2022-05-22 18:16:17 +0300

    create a basic layout for comic information component

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,8 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Info() {
   return (
-    <div>This is the info page</div>
+    <div>
+      <h2>SuperHero</h2>
+      <div className='grid grid-cols-8 mt-6'>
+        <div className='col-start-2 col-span-2'>
+          <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='350px' layout='responsive'/>
+        </div>
+        <div className='col-start-4 col-span-4 px-6'>
+          <div>
+            <h2>Groot</h2>
+            <p className='text-justify'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry 
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
+            of Lorem Ipsum.
+            </p>
+          </div>
+          <p className='mt-8'>Comics: 1200 | Events: 345 | Appearances: 90</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
