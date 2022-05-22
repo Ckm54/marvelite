c416cc9728a9dd1cf58cf6a6147222663a76c58f
Collins <mutaic84@gmail.com>
2022-05-22 16:36:34 +0300

    a pages component to handle better routing using nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import NavBar from '../src/components/NavBar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
