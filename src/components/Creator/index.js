eba73a3e465379a066596e17078349ed5412fc39
Collins <mutaic84@gmail.com>
2022-05-23 00:24:27 +0300

    create a creator component to hold information about a specific comic creator personality

---
+++ src/components/Creator/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Comics from '../Comics'
+
+function CreatorPage() {
+  return (
+    <>
+      <div>CreatorPage</div>
+      <Comics />
+    </>
+  )
+}
+
+export default CreatorPage
