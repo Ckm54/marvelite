24a44623f9f8cff6b39e2b0de6a4214a8a9084c2
Collins <mutaic84@gmail.com>
2022-05-23 00:15:07 +0300

    add footer to global _app to display on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import NavBar from '../src/components/NavBar'
+import Footer from '../src/components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
