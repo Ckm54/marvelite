48e258fa8acb12ae40d00e171742f992d94f81e4
Collins <mutaic84@gmail.com>
2022-05-22 23:19:53 +0300

    float image to the left and text to right

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -12,7 +12,7 @@ function About() {
         <div className='col-start-6 col-span-6 flex flex-col justify-center mt-10 p-6'>
           <div>
             <h2 className='p-4 text-titleRed text-4xl font-normal md:font-bold'>About This Site</h2>
-            <p className='text-justify p-4'>
+            <p className='text-justify p-4 tracking-wide text-lg'>
               The marvel world is vast with lots of rather overwhelming information. To get to learn about the various characters and their specific comics, one can 
               easily be lost. With this site, that has been made a simpler and interractive process as you can now search for your favorite superhero character from 
               the marvel world and get all information including their specific comics and also the faces of the creators behind these comics.
