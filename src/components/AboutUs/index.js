84a8f5be1b13f461e4dfe91438f4f033786a0ee9
Collins <mutaic84@gmail.com>
2022-05-22 22:45:42 +0300

    add styling to the about us component

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -4,19 +4,21 @@ import Link from 'next/link'
 
 function About() {
   return (
-    <div className='max-w-8xl px-5 mx-auto container'>
-      <div className='grid gap-4 md:grid-cols-2'>
-        <Image src={require('../../img/about-us.jpg')} alt='captain' objectFit='cover' layout='responsive' width='500px' height='500px'/>
-        <div className='flex flex-col justify-center mt-10 p-6'>
+    <div>
+      <div className='flex justify-between grid gap-4 md:grid-cols-12'>
+        <div className='col-start-0 col-span-4'>
+          <Image src={require('../../img/about-us.jpg')} alt='captain' objectFit='cover' layout='responsive' width='500px' height='500px'/>
+        </div>
+        <div className='col-start-6 col-span-6 flex flex-col justify-center mt-10 p-6'>
           <div>
-            <h2 className='p-4 text-4xl font-normal md:font-bold'>About This Site</h2>
+            <h2 className='p-4 text-titleRed text-4xl font-normal md:font-bold'>About This Site</h2>
             <p className='text-justify p-4'>
               The marvel world is vast with lots of rather overwhelming information. To get to learn about the various characters and their specific comics, one can 
               easily be lost. With this site, that has been made a simpler and interractive process as you can now search for your favorite superhero character from 
               the marvel world and get all information including their specific comics and also the faces of the creators behind these comics.
             </p>
             <Link href='/'>
-              <button className='float-right mx-5 px-5 py-3 bg-sky-500 hover:bg-sky-500/50'>back to hero search</button>
+              <button className='float-right mx-5 px-5 py-3 bg-titleRed text-white rounded hover:bg-red-600'>back to hero search</button>
             </Link>
           </div>
         </div>
