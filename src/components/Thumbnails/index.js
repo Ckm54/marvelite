acafb07805c0fb33a7758e8528932a9abc12e62d
Collins <mutaic84@gmail.com>
2022-05-22 23:18:34 +0300

    add a zoom effect on hover to thumbnail images

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -7,36 +7,44 @@ function Thumbnails() {
     <div className='max-w-8xl px-5 mx-auto container grid gap-4 grid-cols-4 mt-6'>
       <Link href='/information'>
         <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-          <div>
+          <div className='scale-100 hover:scale-105 ease-in duration-200'>
             <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
           </div>
-          <h4 className='px-4 py-3 text-lg'>captain</h4>
+          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
         </div>
       </Link>
-      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-        <div>
-          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+      <Link href='/information'>
+        <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
+          <div className='scale-100 hover:scale-105 ease-in duration-200'>
+            <Image src={require('../../img/captain.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+          </div>
+          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
         </div>
-        <h4 className='px-4 py-3 text-lg'>captain</h4>
-      </div>
-      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-        <div>
-          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+      </Link>
+      <Link href='/information'>
+        <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
+          <div className='scale-100 hover:scale-105 ease-in duration-200'>
+            <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+          </div>
+          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
         </div>
-        <h4 className='px-4 py-3 text-lg'>captain</h4>
-      </div>
-      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-        <div>
-          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+      </Link>
+      <Link href='/information'>
+        <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
+          <div className='scale-100 hover:scale-105 ease-in duration-200'>
+            <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+          </div>
+          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
         </div>
-        <h4 className='px-4 py-3 text-lg'>captain</h4>
-      </div>
-      <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-        <div>
-          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+      </Link>
+      <Link href='/information'>
+        <div className='bg-white max-w-sm rounded overflow-hidden shadow-lg'>
+          <div className='scale-100 hover:scale-105 ease-in duration-200'>
+            <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+          </div>
+          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
         </div>
-        <h4 className='px-4 py-3 text-lg'>captain</h4>
-      </div>
+      </Link>
     </div>
   )
 }
