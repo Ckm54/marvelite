bbd8fd298a0dec882f2e6929ea286129eeab09f3
Collins <mutaic84@gmail.com>
2022-05-22 17:40:39 +0300

    update styling to the thumbnails container

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -3,12 +3,30 @@ import Image from "next/image"
 
 function Thumbnails() {
   return (
-    <div className='grid gap-4 grid-cols-3 mx-5 mt-6'>
+    <div className='max-w-6xl px-5 mx-auto container grid gap-4 grid-cols-3 mx-5 mt-6'>
       <div className='max-w-sm rounded overflow-hidden shadow-lg'>
         <div>
           <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
         </div>
-        <h4>captain</h4>
+        <h4 className='px-4 py-3 text-lg'>captain</h4>
+      </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <div>
+          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+        </div>
+        <h4 className='px-4 py-3 text-lg'>captain</h4>
+      </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <div>
+          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+        </div>
+        <h4 className='px-4 py-3 text-lg'>captain</h4>
+      </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <div>
+          <Image src={require('../../img/captain.jpg')} alt='captain'  objectFit='cover' width='500px' height='600px' layout='responsive'/>
+        </div>
+        <h4 className='px-4 py-3 text-lg'>captain</h4>
       </div>
       <div className='max-w-sm rounded overflow-hidden shadow-lg'>
         <div>
