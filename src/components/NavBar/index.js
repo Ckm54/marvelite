c32f42a5d3b2a422e2626f426d3c3dcd05dfda9e
Collins <mutaic84@gmail.com>
2022-05-22 22:47:32 +0300

    add overlay image to the to the navbar background

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,17 +5,18 @@ function index() {
   return (
     <nav className='navbar'>
         <ul className='flex justify-between mx-10'>
-          <li className='mt-6'>
+          <li className='mt-6 text-4xl font-semibold text-titleRed'>
             <Link href="/">
               Marvelite
             </Link>
           </li>
-          <li className='mt-6'>
+          <li className='mt-6 text-titleRed font-semibold text-2xl'>
             <Link href="/about">
               About
             </Link>
           </li>
         </ul>
+        <h2 className='text-6xl font-bold mt-20 ml-10 text-headerText'>All your SuperHero <br /> info in one place</h2>
     </nav>
   )
 }
