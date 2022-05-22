fad8987607b59229e9193f7c3b70ad69f49aa7a3
Collins <mutaic84@gmail.com>
2022-05-23 01:25:34 +0300

    add a focus style to input search field

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Search() {
   return (
     <form className='mt-3 md:mt-0'>
-      <input className="nd:font-semibold md:text-lg text-white border-b-4 border-titleRed md:px-2 md:mr-3 focus:outline-none bg-transparent placeholder-gray-500" placeholder='Input superhero name' type='text' />
+      <input className="nd:font-semibold md:text-lg text-white border-b-4 border-titleRed md:px-2 md:mr-3 focus:outline-none bg-transparent placeholder-gray-500 focus:border-red-600" placeholder='Input superhero name' type='text' />
       <button className='md:text-2xl bg-titleRed font-semibold text-white px-4 py-1 rounded hover:bg-red-600'>Search</button>
     </form>
   )
