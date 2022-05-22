ddb9d64aa92b7047b0b63cf25f559e1a1f58e0df
Collins <mutaic84@gmail.com>
2022-05-22 23:18:58 +0300

    update styling to info component

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -5,23 +5,22 @@ import Comics from '../Comics'
 function Info() {
   return (
     <div>
-      <h2>SuperHero</h2>
+      <h2 className='text-titleRed font-semibold text-2xl pl-4 mt-3 ml-10'>SuperHero:</h2>
       <div className='grid grid-cols-8 mt-6'>
         <div className='col-start-2 col-span-2'>
-          <Image src={require('../../img/groot.jpg')} alt='captain' objectFit='cover' width='500px' height='350px' layout='responsive'/>
+          <Image src={require('../../img/groot.jpg')} alt='captain' objectFit='cover' width='500px' height='400px' layout='responsive'/>
         </div>
-        <div className='col-start-4 col-span-4 px-6'>
-          <div>
-            <h2>Groot</h2>
-            <p className='text-justify'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry 
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
-            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-            of Lorem Ipsum.
-            </p>
-          </div>
+        <div className='col-start-4 col-span-4 px-6 bg-white py-6'>
+          <h2 className='font-bold text-2xl'>Groot</h2>
+          <p className='text-justify'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry 
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+          a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
+          of Lorem Ipsum.
+          
+          </p>
           <p className='mt-8'>Comics: 1200 | Events: 345 | Appearances: 90</p>
         </div>
       </div>
