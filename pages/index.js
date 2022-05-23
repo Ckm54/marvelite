097fe181a91c3a006ddd3095def04b15a12b4e28
Collins <mutaic84@gmail.com>
2022-05-23 09:52:02 +0300

    simulate data fetch from a mock api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import styles from '../styles/Home.module.css'
 import Main from '../src/components/Main'
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <Main />
+    <Main characters={props.results}/>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch(`https://www.superheroapi.com/api.php/508656130959573/search/b`)
+  const data = await response.json()
+  return {
+    props: data
+  }
 }
