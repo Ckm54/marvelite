05fbfdc2951d2fd5e6d8ef926d20bdc1721a15ce
Collins <mutaic84@gmail.com>
2022-05-23 10:49:06 +0300

    pass character props and display fetched character data

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Search from './Search'
 import Thumbnails from './Thumbnails'
 
-function Main() {
+function Main({characters}) {
   return (
     <div>
       <div className='flex flex-col justify-between mx-10 mt-4 md:flex-row'>
         <h2 className='text-xl text-titleRed font-semibold md:text-2xl'>Common Superheroes</h2>
         <Search />
       </div>
-      <Thumbnails />
+      <Thumbnails heroes={characters}/>
     </div>
   )
 }
--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -2,49 +2,21 @@ import React from 'react'
 import Image from "next/image"
 import Link from 'next/link'
 
-function Thumbnails() {
+function Thumbnails({heroes}) {
+  // const superheroData = heroes.characters.results
+  console.log(heroes)
   return (
     <div className='md:max-w-8xl md:px-5 mx-auto container md:grid gap-4 grid-cols-4 mt-6'>
-      <Link href='/character'>
-        <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
-            <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+      {heroes.map((hero) => (
+        <Link href='/character' key={hero.id}>
+          <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
+            <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
+              <Image src={`${hero.image.url}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+            </div>
+            <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>{hero.name}</h4>
           </div>
-          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
-        </div>
-      </Link>
-      <Link href='/character'>
-        <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
-            <Image src={require('../../img/captain.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
-          </div>
-          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
-        </div>
-      </Link>
-      <Link href='/character'>
-        <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
-            <Image src={require('../../img/thanos.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
-          </div>
-          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
-        </div>
-      </Link>
-      <Link href='/character'>
-        <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
-            <Image src={require('../../img/img3.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
-          </div>
-          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
-        </div>
-      </Link>
-      <Link href='/character'>
-        <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
-            <Image src={require('../../img/ironman.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
-          </div>
-          <h4 className='px-4 py-3 text-lg font-semibold text-2xl'>captain</h4>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </div>
   )
 }
