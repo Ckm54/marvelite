94d312d27d5a296b41748d686df0d28079cfd08e
Collins <mutaic84@gmail.com>
2022-05-23 10:48:26 +0300

    add domain name for image loading

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['www.superherodb.com']
+  }
+}
