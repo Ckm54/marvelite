0e49db80c60f7903b9bd573bd19e29c3cb8105de
Collins <mutaic84@gmail.com>
2022-05-23 20:57:52 +0300

    apply hover effect to card instead of only card image

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -16,10 +16,8 @@ function Thumbnails({characters}) {
   return (
     <div className='md:max-w-8xl md:px-5 mx-auto container md:grid gap-4 grid-cols-4 mt-6'>
       {heroes.map((hero) => loading ? <Loader /> : <Link href='/character' key={hero.id}>
-          <div className='mx-auto md:mx-0 bg-white max-w-sm rounded overflow-hidden shadow-lg'>
-            <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer'>
-              <Image src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
-            </div>
+          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer mx-auto md:mx-0 bg-white mt-2 max-w-sm rounded overflow-hidden shadow-lg'>
+            <Image src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
             <h4 className='px-4 py-3 text-lg font-semibold text-xl'>{hero.name}</h4>
             <p className='px-4 py-2 bg-gray-300'>{characters.attributionText}</p>
           </div>
