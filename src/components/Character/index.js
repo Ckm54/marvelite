aa7420b98bbadc13f174a61c3290113ec8d6b912
Collins <mutaic84@gmail.com>
2022-05-24 21:40:59 +0300

    display character info on card and style card

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -13,20 +13,23 @@ function Info({ character }) {
         <div className='col-start-2 col-span-2'>
           <Image src={`${heroInfo.thumbnail.path}.${heroInfo.thumbnail.extension}`} alt='captain' objectFit='fill' width='300px' height='300px' layout='responsive'/>
         </div>
-        <div className='col-start-4 col-span-4 px-6 bg-white py-6'>
-          <h2 className='font-bold text-2xl mb-5'>{heroInfo.name}</h2>
-          <p className='text-justify'>
-          {heroInfo.description}
-          </p>
-          <p className='mt-8'>Comics: {heroInfo.comics.available} | Events: {heroInfo.events.available} | Stories: {heroInfo.stories.available}</p>
-          <p className='mt-8 text-lg text-titleRed font-medium'>
-            Comics:
-            <Link href='/comicdetail'>
-              <span className='pl-4 font-normal italic'>{
-                heroInfo.comics.items.map((comic) => <span key={comic.name} className="mr-5 hover:cursor-pointer hover:text-red-600">{comic.name} ,</span>)
-              }</span>
-            </Link>
-          </p>
+        <div className='flex flex-col justify-between col-start-4 col-span-4 bg-white pt-6'>
+          <div className='px-8'>
+            <h2 className='font-bold text-2xl mb-5'>{heroInfo.name}</h2>
+            <p className='text-justify'>
+            {heroInfo.description}
+            </p>
+            <p className='mt-4'>Comics: {heroInfo.comics.available} | Events: {heroInfo.events.available} | Stories: {heroInfo.stories.available}</p>
+            <p className='mt-4 text-lg text-titleRed font-medium'>Comics:</p>
+            <p className='text-titleRed text-justify'>
+              <Link href='/comicdetail'>
+                <span className='pl-4 font-normal italic'>{
+                  heroInfo.comics.items.map((comic) => <span key={comic.name} className="mr-5 hover:cursor-pointer hover:text-red-600">{comic.name} ,</span>)
+                }</span>
+              </Link>
+            </p>
+          </div>
+          <p className='bg-gray-200'>{character.attributionText}</p>
         </div>
       </div>
       <Comics />
