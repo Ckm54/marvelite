8b8a601f0a1ddaa4d005df1cd5f646b131a712b6
Collins <mutaic84@gmail.com>
2022-05-24 11:22:20 +0300

    create functions to handle searching capability

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Search() {
+function Search({ filterData }) {
+  const [searchQuery, setSearchQuery] = useState('')
+  function handleChange(e) {
+    setSearchQuery(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+   filterData(searchQuery)
+  }
   return (
-    <form className='mt-3 md:mt-0'>
-      <input className="nd:font-semibold md:text-lg border-b-4 border-titleRed md:px-2 md:mr-3 focus:outline-none bg-transparent placeholder-gray-500 focus:border-red-600" placeholder='Input superhero name' type='text' />
-      <button className='md:text-2xl bg-titleRed font-semibold text-white px-4 py-1 rounded hover:bg-red-600'>Search</button>
+    <form className='mt-3 md:mt-0' onSubmit={handleSubmit}>
+      <input className="nd:font-semibold md:text-lg border-b-4 border-titleRed md:px-2 md:mr-3 focus:outline-none bg-transparent placeholder-gray-500 focus:border-red-600" placeholder='Input superhero name' type='text' onChange={handleChange} value={searchQuery}/>
+      <input type="submit" className='md:text-2xl bg-titleRed font-semibold text-white px-4 py-1 rounded hover:bg-red-600' value="Search"/>
     </form>
   )
 }
