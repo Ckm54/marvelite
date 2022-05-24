f3241003036b482e492d603e66db95ed98071233
Collins <mutaic84@gmail.com>
2022-05-24 11:23:35 +0300

    handle state here to set searching

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './Search'
 import Thumbnails from './Thumbnails'
 
 function Main({characters}) {
+  const[loading, setLoading] = useState(false)
+
+  console.log(characters)
+  const[heroes, setHeroes] = useState([])
+  useEffect(() => {
+    setHeroes(characters.data.results)
+  }, [characters])
+
+  function handleHeroSearch(searchTerm) {
+    console.log("in the main component: ", searchTerm)
+  }
   return (
     <div>
       <div className='flex flex-col justify-between mx-10 mt-4 md:flex-row'>
         <h2 className='text-xl text-titleRed font-semibold md:text-2xl'>Common Superheroes</h2>
-        <Search />
+        <Search filterData={handleHeroSearch}/>
       </div>
-      <Thumbnails characters={characters}/>
+      <Thumbnails heroes={heroes} loading={loading} attribution={characters.attributionText}/>
     </div>
   )
 }
