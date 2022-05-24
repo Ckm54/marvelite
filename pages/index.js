c4bd732c7ad6fe502252c8a16c518a215ae652f3
Collins <mutaic84@gmail.com>
2022-05-24 12:08:21 +0300

    handle creating hash in external file function

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
 import Main from '../src/components/Main'
-import md5 from 'md5'
-import { data } from 'autoprefixer'
+import { ts, hash, PUBLIC_KEY } from './api/hello'
 
-const PUBLIC_KEY = '1bc98542b53a9d850d459282d676e3e1'
-const PRIVATE_KEY = '9aec4e4a418f0cb25dcf0278924e50d7e670386e'
 
 export default function Home(props) {
   // const [heroes, setHeroes] = useState([])
@@ -23,8 +18,7 @@ export default function Home(props) {
 }
 
 export async function getServerSideProps(context) {
-  const ts = Number(new Date())
-  const hash = md5(ts + PRIVATE_KEY + PUBLIC_KEY)
+  
   const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&&limit=24&apikey=${PUBLIC_KEY}&hash=${hash}`)
   const data = await response.json()
 
