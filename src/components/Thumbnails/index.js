28a6e1f1474679222f6c5038c4121283f1de953e
Collins <mutaic84@gmail.com>
2022-05-24 11:23:08 +0300

    lift state up to the parent component

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -1,16 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Image from "next/image"
 import Link from 'next/link'
 import Loader from '../Loader'
 
-function Thumbnails({characters}) {
-  const[loading, setLoading] = useState(false)
-  console.log(characters)
-  const[heroes, setHeroes] = useState([])
-  useEffect(() => {
-    setHeroes(characters.data.results)
-  }, [characters])
-
+function Thumbnails({heroes, loading, attribution}) {
 
   console.log(loading)
   return (
@@ -19,7 +12,7 @@ function Thumbnails({characters}) {
           <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer mx-auto md:mx-0 bg-white mt-2 max-w-sm rounded overflow-hidden shadow-lg'>
             <Image src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
             <h4 className='px-4 py-3 text-lg font-semibold text-xl'>{hero.name}</h4>
-            <p className='px-4 py-2 bg-gray-300'>{characters.attributionText}</p>
+            <p className='px-4 py-2 bg-gray-300'>{attribution}</p>
           </div>
         </Link>)}
     </div>
