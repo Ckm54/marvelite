de265a4a5ca81a1d14de1d6aba0f8b18486fe480
Collins <mutaic84@gmail.com>
2022-05-24 14:04:44 +0300

    create a text to display when a search is activated

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import { ts, hash, PUBLIC_KEY } from "../../pages/api/hello"
 
 function Main({characters}) {
   const[loading, setLoading] = useState(false)
+  const [query, setQuery] = useState('')
 
   console.log(characters)
   const[heroes, setHeroes] = useState([])
@@ -17,14 +18,15 @@ function Main({characters}) {
     const searchResults = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&&nameStartsWith=${searchTerm}&orderBy=name&apikey=${PUBLIC_KEY}&hash=${hash}`).then(res => res.json())
     console.log("in the main component: ", searchTerm)
     setHeroes(searchResults.data.results)
+    setQuery(searchTerm)
   }
   return (
     <div>
       <div className='flex flex-col justify-between mx-10 mt-4 md:flex-row'>
-        <h2 className='text-xl text-titleRed font-semibold md:text-2xl'>Common Superheroes</h2>
+        <h2 className='text-xl text-titleRed font-semibold md:text-2xl'>Common Superheroes {query ? `Search Results For ${query}...` : null}</h2>
         <Search filterData={handleHeroSearch}/>
       </div>
-      <Thumbnails heroes={heroes} loading={loading} attribution={characters.attributionText}/>
+      <Thumbnails heroes={heroes} loading={loading} attribution={characters.attributionText} />
     </div>
   )
 }
