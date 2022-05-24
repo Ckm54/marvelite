4d40139ca1bfa4d27448519bb665b0c140a0f1d2
Collins <mutaic84@gmail.com>
2022-05-24 14:10:16 +0300

    clear form fields on submit

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -13,7 +13,7 @@ function Search({ filterData }) {
   }
   return (
     <form className='mt-3 md:mt-0' onSubmit={handleSubmit}>
-      <input className="nd:font-semibold md:text-lg border-b-4 border-titleRed md:px-2 md:mr-3 focus:outline-none bg-transparent placeholder-gray-500 focus:border-red-600" placeholder='Input superhero name' type='text' onChange={handleChange} value={searchQuery}/>
+      <input className="md:font-semibold md:text-lg border-b-4 border-titleRed md:px-2 md:mr-3 focus:outline-none bg-transparent placeholder-gray-500 focus:border-red-600" placeholder='Input superhero name' type='text' onChange={handleChange} value={searchQuery}/>
       <input type="submit" className='md:text-2xl bg-titleRed font-semibold text-white px-4 py-1 rounded hover:bg-red-600' value="Search"/>
     </form>
   )
