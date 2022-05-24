28e76ebc30e8af7dcf0b88b364318b729d218383
Collins <mutaic84@gmail.com>
2022-05-24 19:16:55 +0300

    make thumbnail route link dynamic

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -8,7 +8,10 @@ function Thumbnails({ heroes, loading, attribution }) {
   console.log(loading)
   return (
     <div className='md:max-w-8xl md:px-5 mx-auto container md:grid gap-4 grid-cols-4 mt-6'>
-      {heroes.map((hero) => loading ? <Loader /> : <Link href='/character' key={hero.id}>
+      {heroes.map((hero) => loading ? <Loader /> : <Link href={{
+        pathname: '/character',
+        query: hero.id
+      }} as={`/character/${hero.id}`} key={hero.id}>
           <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer mx-auto md:mx-0 bg-white mt-2 max-w-sm rounded overflow-hidden shadow-lg'>
             <Image src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
             <h4 className='px-4 py-3 text-lg font-semibold text-xl'>{hero.name}</h4>
