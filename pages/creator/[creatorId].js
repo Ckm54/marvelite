1a979936b3c39e0cc67ab909df577dc63e32cdb3
Collins <mutaic84@gmail.com>
2022-05-25 15:00:28 +0300

    fetch comics specisic to a comic creator and pass as props to the comics component

--- pages/creator/[creatorId].js
+++ pages/creator/[creatorId].js
@@ -2,23 +2,28 @@ import React from 'react'
 import CreatorPage from '../../src/components/Creator'
 import { ts, PUBLIC_KEY, hash } from "../api/hello"
 
-export default function Creator({ creatorData }) {
+export default function Creator({ creatorData, comicData }) {
 
-  console.log(creatorData)
+  console.log("comic data: ", comicData)
+
   return (
-    <CreatorPage creatorData={creatorData}/>
+    <CreatorPage creatorData={creatorData} comicData={comicData}/>
   )
 }
 
 export async function getServerSideProps(context) {
   const dataIdObj = context.query
   const id = Number(dataIdObj.creatorId)
-  const response = await fetch(`https://gateway.marvel.com:443/v1/public/creators/${id}?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
-  const creatorData = await response.json()
+  const creatorResponse = await fetch(`https://gateway.marvel.com:443/v1/public/creators/${id}?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  const creatorData = await creatorResponse.json()
+
+  const comicResponse = await fetch(`https://gateway.marvel.com:443/v1/public/creators/${id}/comics?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  const comicData = await comicResponse.json()
 
   return {
     props: {
-      creatorData: creatorData
+      creatorData: creatorData,
+      comicData: comicData
     }
   }
 }
