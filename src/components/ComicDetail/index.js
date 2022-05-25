dbd68e3e2e1d7db990cdfc1fcdb1ff3531503316
Collins <mutaic84@gmail.com>
2022-05-25 15:41:09 +0300

    display attribution text within component as required by the api

--- src/components/ComicDetail/index.js
+++ src/components/ComicDetail/index.js
@@ -4,8 +4,6 @@ import Link from 'next/link'
 
 function ComicDetail({ comicData }) {
 
-  // console.log("in comic details component: ", comicData)
-
   const comic = comicData.data.results[0]
 
   console.log(comic)
@@ -16,38 +14,41 @@ function ComicDetail({ comicData }) {
         <div className='col-start-0 col-span-2'>
           <Image src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt='captain' objectFit='fill' width='300px' height='300px' layout='responsive'/>
         </div>
-        <div className='col-start-3  col-span-4 p-10 bg-white'>
-          <h2 className='font-bold text-2xl'>Format: <span>{comic.format}</span></h2>
-          <p className='text-justify text-lg tracking-wide' dangerouslySetInnerHTML={{
-            __html: `${comic.description}`
-          }} style={{whiteSpace: "pre-line"}}>
-          </p>
-          <p className='mt-8 text-lg text-titleRed font-medium'>
-            Characters:
-            {comic.characters.items.map((character) => {
-              return (
-                <Link key={character.resourceURI} href={{
-                  pathname: '/character',
-                  query: (character.resourceURI.split('/').pop())
-                }} as={`/character/${(character.resourceURI.split('/').pop())}`}>
-                  <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{character.name},</span>
-                </Link>
-              )
-            })}
-          </p>
-          <p className='mt-8 text-lg text-titleRed font-medium'>
-            Creators:
-            {comic.creators.items.map((creator) => {
-              return (
-                <Link key={creator.resourceURI} href={{
-                  pathname: '/creator',
-                  query: (creator.resourceURI.split('/').pop())
-                }} as={`/creator/${(creator.resourceURI.split('/').pop())}`}>
-                  <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{creator.name},</span>
-                </Link>
-              )
-            })}
-          </p>
+        <div className='col-start-3 flex flex-col justify-between col-span-4 bg-white'>
+          <div className='p-10'>
+            <h2 className='font-bold text-2xl'>Format: <span>{comic.format}</span></h2>
+            <p className='text-justify text-lg tracking-wide' dangerouslySetInnerHTML={{
+              __html: `${comic.description}`
+            }} style={{whiteSpace: "pre-line"}}>
+            </p>
+            <p className='mt-8 text-lg text-titleRed font-medium'>
+              Characters:
+              {comic.characters.items.map((character) => {
+                return (
+                  <Link key={character.resourceURI} href={{
+                    pathname: '/character',
+                    query: (character.resourceURI.split('/').pop())
+                  }} as={`/character/${(character.resourceURI.split('/').pop())}`}>
+                    <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{character.name},</span>
+                  </Link>
+                )
+              })}
+            </p>
+            <p className='mt-8 text-lg text-titleRed font-medium'>
+              Creators:
+              {comic.creators.items.map((creator) => {
+                return (
+                  <Link key={creator.resourceURI} href={{
+                    pathname: '/creator',
+                    query: (creator.resourceURI.split('/').pop())
+                  }} as={`/creator/${(creator.resourceURI.split('/').pop())}`}>
+                    <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{creator.name},</span>
+                  </Link>
+                )
+              })}
+            </p>
+          </div>
+          <p className='bg-gray-200 pl-4 py-2 text-titleRed text-xl'>{comicData.attributionText}</p>
         </div>
       </div>
     </div>
