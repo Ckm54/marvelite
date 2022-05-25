72c7e74d7daef5254d6d578d3f2fc2e4b07eaad8
Collins <mutaic84@gmail.com>
2022-05-25 19:48:48 +0300

    add contact info

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-    <footer className='bg-black h-40 mt-10 p-5'>
+    <footer className='bg-black h-40 mt-10 py-5 px-10'>
       <Link href="/">
         <h3 className='text-red-500 font-bold text-4xl hover:cursor-pointer hover:text-red-600'>Marvelite</h3>
       </Link>
-      <p className='text-white py-4'>Developer Contact</p>
+      <p className='text-white py-4'>Designed & created by: <span className='italic text-titleRed'>Collins Kiplangat</span> &copy; 2022</p>
     </footer>
   )
 }
