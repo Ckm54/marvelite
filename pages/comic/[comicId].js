e9c8cfb184f1edcd0c405fc15a11166ec413b325
Collins <mutaic84@gmail.com>
2022-05-25 11:17:19 +0300

    new file to handle data fetch for a specific comis

---
+++ pages/comic/[comicId].js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ComicDetail from '../../src/components/ComicDetail'
+import { ts, PUBLIC_KEY, hash } from "../api/hello"
+
+export default function Detail({ comicData }) {
+  return (
+    <ComicDetail comicData={comicData}/>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const dataIdObj = context.query
+  const id = Number(dataIdObj.comicId)
+  const response = await fetch(`https://gateway.marvel.com:443/v1/public/comics/${id}?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  const comicData = await response.json()
+
+  return {
+    props: {
+      comicData: comicData
+    }
+  }
+}
