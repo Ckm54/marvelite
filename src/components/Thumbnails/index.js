15e3329463a9d0e5af98045700c072a4034abc4e
Collins <mutaic84@gmail.com>
2022-05-25 16:06:54 +0300

    update styling

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -12,10 +12,12 @@ function Thumbnails({ heroes, loading, attribution }) {
         pathname: '/character',
         query: hero.id
       }} as={`/character/${hero.id}`} key={hero.id}>
-          <div className='scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer mx-auto md:mx-0 bg-white mt-2 max-w-sm rounded overflow-hidden shadow-lg'>
-            <Image src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
-            <h4 className='px-4 py-3 text-lg font-semibold text-xl'>{hero.name}</h4>
-            <p className='px-4 py-2 bg-gray-300'>{attribution}</p>
+          <div className='flex flex-col justify-between scale-100 hover:scale-105 ease-in duration-200 hover:cursor-pointer mx-auto md:mx-0 bg-white mt-2 max-w-sm rounded overflow-hidden shadow-lg'>
+            <div>
+              <Image src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+              <h4 className='px-4 py-3 text-lg font-semibold text-xl'>{hero.name}</h4>
+            </div>
+            <p className='px-4 py-2 bg-gray-300 text-titleRed'>{attribution}</p>
           </div>
         </Link>)}
     </div>
