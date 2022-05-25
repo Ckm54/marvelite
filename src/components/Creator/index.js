c540b66ebc82df81cd14e418d89e5dea7c5a1bed
Collins <mutaic84@gmail.com>
2022-05-25 12:59:59 +0300

    display a creato's info on the creators component

--- src/components/Creator/index.js
+++ src/components/Creator/index.js
@@ -3,35 +3,38 @@ import Comics from '../Comics'
 import Image from 'next/image'
 import Link from 'next/link'
 
-function CreatorPage() {
+function CreatorPage({ creatorData }) {
+  console.log(creatorData)
+  const creator = creatorData.data.results[0]
   return (
     <>
       <h2 className='text-titleRed font-semibold text-2xl pl-4 mt-3 md:ml-10'>Creator:</h2>
       <div className='md:grid grid-cols-8 mt-6'>
         <div className='col-start-2 col-span-2'>
-          <Image src={require('../../img/stan.jpeg')} alt='captain' objectFit='fill' width='300px' height='300px' layout='responsive'/>
+          <Image src={`${creator.thumbnail.path}.${creator.thumbnail.extension}`} alt='captain' objectFit='fill' width='300px' height='300px' layout='responsive'/>
         </div>
         <div className='col-start-4 col-span-4 px-6 bg-white py-6'>
-          <h2 className='font-bold text-2xl'>Stan Lee</h2>
+          <h2 className='font-bold text-2xl'>{creator.fullName}</h2>
           <p className='text-justify'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry 
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
-          a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum.
+          Lorem 
           
           </p>
-          <p className='mt-8'>Comics: 1200 | Events: 345 | Appearances: 90</p>
           <p className='mt-8 text-lg text-titleRed font-medium'>
-            Characters:
-            <Link href='/character'>
-              <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>Groot</span>
+            Comics:
+            <br />
+            {creator.comics.items.map((comic) => (
+              <Link key={comic.name} href={{
+                pathname: '/comicdetail',
+                query: (comic.resourceURI.split('/').pop())
+                }} as={`/comic/${(comic.resourceURI.split('/').pop())}`}>
+              <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{comic.name},</span>
             </Link>
+            ))}
           </p>
+          <p className='mt-8'>Comics: {creator.comics.available} | Events: {creator.events.available} | Series: {creator.series.available} | Stories: {creator.stories.available}</p>
         </div>
       </div>
-      <Comics />
+      {/* <Comics characterComics={creatorData}/> */}
     </>
   )
 }
