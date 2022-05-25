05f4d9e27b83812f1b5f6ac8377347071ff796bd
Collins <mutaic84@gmail.com>
2022-05-25 16:07:29 +0300

    update styling to comics component

--- src/components/Comics/index.js
+++ src/components/Comics/index.js
@@ -6,7 +6,7 @@ function Comics({ characterComics }) {
   return (
     <div>
       <h3 className='text-titleRed font-semibold text-2xl pl-4 mt-3 md:ml-10'>Comics:</h3>
-      <p className='pl-4 md:ml-10 text-red-800 font-normal text-lg'>Only showing comics with a description...</p>
+      <p className='pl-4 md:ml-10 text-red-800 font-normal text-lg'>{characterComics.length > 0 ? "Only showing comics with a description..." : "No available comics for this character"}</p>
       <div className='max-w-10xl px-5 mx-auto container md:grid gap-2 grid-cols-3 mt-2'>
         {characterComics.map((comic) => {
           if(comic.description) {
@@ -16,7 +16,7 @@ function Comics({ characterComics }) {
                 <Image src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
               </div>
               <div className='px-5 text-justify mb-7 pb-5'>
-                <h4 className='py-3 text-lg'>{comic.title}</h4>
+                <h4 className='py-3 text-lg font-semibold text-titleRed'>{comic.title}</h4>
                 <p dangerouslySetInnerHTML={{
                     __html: `${comic.description}`
                   }} style={{whiteSpace: "pre-line"}}>
