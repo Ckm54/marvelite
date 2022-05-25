6ade1b0f79710d99afa159f54c60e0fd1af9a656
Collins <mutaic84@gmail.com>
2022-05-25 11:15:59 +0300

    handle displaying a specific comic's data deails

--- src/components/ComicDetail/index.js
+++ src/components/ComicDetail/index.js
@@ -2,35 +2,44 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-function ComicDetail() {
+function ComicDetail({ comicData }) {
+
+  // console.log("in comic details component: ", comicData)
+
+  const comic = comicData.data.results[0]
+
+  console.log(comic)
   return (
     <div className='flex flex-col mx-20 px-10'>
-      <h2 className='text-titleRed font-semibold text-3xl mt-3 '>This is the comic title:</h2>
+      <h2 className='text-titleRed font-semibold text-3xl mt-3 '>{comic.title}</h2>
       <div className='md:grid grid-cols-6 mt-6'>
         <div className='col-start-0 col-span-2'>
-          <Image src={require('../../img/Gotg_2.jpg')} alt='captain' objectFit='fill' width='300px' height='300px' layout='responsive'/>
+          <Image src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt='captain' objectFit='fill' width='300px' height='300px' layout='responsive'/>
         </div>
         <div className='col-start-3  col-span-4 p-10 bg-white'>
-          <h2 className='font-bold text-2xl'>Specific title Here</h2>
+          <h2 className='font-bold text-2xl'>Format: <span>{comic.format}</span></h2>
           <p className='text-justify text-lg tracking-wide'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry 
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
-          a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-          of Lorem Ipsum.
+          {comic.description}
           </p>
           <p className='mt-8 text-lg text-titleRed font-medium'>
             Characters:
-            <Link href='/character'>
-              <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>Groot</span>
-            </Link>
+            {comic.characters.items.map((character) => {
+              return (
+                <Link key={character.resourceURI} href='/character'>
+                  <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{character.name},</span>
+                </Link>
+              )
+            })}
           </p>
           <p className='mt-8 text-lg text-titleRed font-medium'>
             Creators:
-            <Link href='/creator'>
-              <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>Stan Lee</span>
-            </Link>
+            {comic.creators.items.map((creator) => {
+              return (
+                <Link key={creator.resourceURI} href='/creator'>
+                  <span className='pl-4 font-normal italic hover:cursor-pointer hover:text-red-600'>{creator.name},</span>
+                </Link>
+              )
+            })}
           </p>
         </div>
       </div>
