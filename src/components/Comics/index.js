2f5298d5b2cc596b921f6af6e2bd6a04a8796722
Collins <mutaic84@gmail.com>
2022-05-25 15:02:31 +0300

    use comic data as an array instead of an object

--- src/components/Comics/index.js
+++ src/components/Comics/index.js
@@ -3,14 +3,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 function Comics({ characterComics }) {
-  const comicData = characterComics.data.results
-  
   return (
     <div>
       <h3 className='text-titleRed font-semibold text-2xl pl-4 mt-3 md:ml-10'>Comics:</h3>
       <p className='pl-4 md:ml-10 text-red-800 font-normal text-lg'>Only showing comics with a description...</p>
       <div className='max-w-10xl px-5 mx-auto container md:grid gap-2 grid-cols-3 mt-2'>
-        {comicData.map((comic) => {
+        {characterComics.map((comic) => {
           if(comic.description) {
             return (
               <div key={comic.id} className='max-w-sm rounded overflow-hidden shadow-lg mt-8 bg-white scale-100 hover:scale-105 ease-in duration-200'>
