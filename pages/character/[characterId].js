f5c8fa5f6e90f99e62da1242f4d8be00ca9fc3cb
Collins <mutaic84@gmail.com>
2022-05-25 09:48:30 +0300

    fetch a character comic based on the character's id

--- pages/character/[characterId].js
+++ pages/character/[characterId].js
@@ -1,10 +1,10 @@
 import Info from "../../src/components/Character"
 import { ts, PUBLIC_KEY, hash } from "../api/hello"
 
-export default function index({ character }) {
+export default function index({ character, characterComics }) {
 
   return (
-    <Info character={character}/>
+    <Info character={character} characterComics={characterComics}/>
   )
 }
 
@@ -14,10 +14,13 @@ export async function getServerSideProps(context) {
   const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
   const character = await response.json()
 
-  console.log(character)
+  const comicResponse = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}/comics?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  const characterComics = await comicResponse.json()
+
   return {
     props: {
-      character: character
+      character: character,
+      characterComics
     }
   }
 }
