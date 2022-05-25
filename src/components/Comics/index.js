74a1889ee97ae436f9ff5d4bc0b6bc0b10c205e6
Collins <mutaic84@gmail.com>
2022-05-25 10:02:11 +0300

    add an explanation of the comic data being shown

--- src/components/Comics/index.js
+++ src/components/Comics/index.js
@@ -9,13 +9,14 @@ function Comics({ characterComics }) {
   return (
     <div>
       <h3 className='text-titleRed font-semibold text-2xl pl-4 mt-3 md:ml-10'>Comics:</h3>
+      <p className='pl-4 md:ml-10 text-red-800 font-normal text-lg'>Only showing comics with a description...</p>
       <div className='max-w-10xl px-5 mx-auto container md:grid gap-2 grid-cols-3 mt-2'>
         {comicData.map((comic) => {
           if(comic.description) {
             return (
               <div key={comic.id} className='max-w-sm rounded overflow-hidden shadow-lg mt-8 bg-white scale-100 hover:scale-105 ease-in duration-200'>
               <div>
-                <Image src={require('../../img/Gotg_2.jpg')} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
+                <Image src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt='captain' objectFit='cover' width='500px' height='600px' layout='responsive'/>
               </div>
               <div className='px-5 text-justify mb-7 pb-5'>
                 <h4 className='py-3 text-lg'>{comic.title}</h4>
