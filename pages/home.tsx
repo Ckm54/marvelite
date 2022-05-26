396e5c26d0d92787447955fe8543a936b652d4e5
Collins <mutaic84@gmail.com>
2022-05-27 02:40:03 +0300

    make this the base url

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import Main from "../src/components/Main";
+import { ts, hash, PUBLIC_KEY } from './api/hello'
 
-const Home = () => {
+
+const Home = (props) => {
   return (
     <div>
-      <h1>This is the Homepage</h1>
+      <Main characters={props}/>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+
+  function generateLetter() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+  }
+  
+  const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&&nameStartsWith=${generateLetter()}&&limit=24&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  const data = await response.json()
+
+  return {
+    props: data
+  }
+  
 }
 
 export default Home;
