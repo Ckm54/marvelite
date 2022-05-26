d9fd50980cfab44b5ffe8a461b91d193423f2b27
Collins <mutaic84@gmail.com>
2022-05-27 02:40:36 +0300

    style login and sign up forms

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,6 +5,7 @@ import React, {useState} from "react";
 const Login = () => {
   const router = useRouter()
   const { user, login } = useAuth()
+  const [err, setErr] = useState(false)
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -13,12 +14,12 @@ const Login = () => {
   const handleLogin = async (e: any) => {
     e.preventDefault();
 
-    console.log("logged in: ", user)
+    
       try {
         await login(data.email, data.password)
         router.push('/home')
       } catch(err) {
-        console.log(err)
+        console.log("error")
       }
   
       setData({
@@ -26,20 +27,27 @@ const Login = () => {
         email: "",
         password: "",
       })
+
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <label>Email</label>
-        <input type="email" name="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+    <div className="flex py-[20%] md:py-[10%] items-center justify-center bg-gray-100">
+      <div className="px-8 py-6 text-left bg-white shadow-lg">
+        <h1 className="text-2xl font-bold text-center text-titlered">Login</h1>
+        <div className="mt-4">
+          <form onSubmit={handleLogin}>
+            <label className="block">Email</label>
+            <input className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600" type="email" name="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
 
-        <label>Password</label>
-        <input type="password" name="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
-
-        <button type="submit">Login</button>
-      </form>
+            <label className="block">Password</label>
+            <input className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600" type="password" name="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+            <div className="flex items-baseline justify-between">
+              <button className="px-6 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-700" type="submit">Login</button>
+            </div>
+          </form>
+          {err ? <p className="my-4 text-red-400 text-normal">Error</p> : null}
+        </div>
+      </div>
     </div>
   )
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext"
 
 const Signup = () => {
   const { user, signup } = useAuth()
-  console.log(user)
+  
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -25,17 +25,23 @@ const Signup = () => {
   }
 
   return (
-    <div>
-      <h1>Signup</h1>
-      <form onSubmit={handleSignup}>
-        <label>Email</label>
-        <input type="email" name="email" required value={data.email} onChange={(e: any) => setData({...data, email: e.target.value})} />
+    <div className="flex py-[20%] md:py-[10%] items-center justify-center bg-gray-100">
+      <div className="px-8 py-6 text-left bg-white shadow-lg">
+        <h1 className="text-2xl font-bold text-center text-titleRed">Signup</h1>
+        <div className="mt-4">
+          <form onSubmit={handleSignup}>
+            <label className="block">Email</label>
+            <input className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600" type="email" name="email" required value={data.email} onChange={(e: any) => setData({...data, email: e.target.value})} />
 
-        <label>Password</label>
-        <input type="password" name="password" required value={data.password} onChange={(e: any) => setData({...data, password: e.target.value})} />
+            <label className="block">Password</label>
+            <input className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600" type="password" name="password" required value={data.password} onChange={(e: any) => setData({...data, password: e.target.value})} />
 
-        <button type="submit">Signup</button>
-      </form>
+            <div className="flex items-baseline justify-between">
+              <button className="px-6 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-700" type="submit">Signup</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
