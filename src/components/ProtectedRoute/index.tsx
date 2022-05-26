10211f01f321ba12639ceafc9d895d2131b1c4f5
Collins <mutaic84@gmail.com>
2022-05-27 01:43:11 +0300

    if no user push to login page

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useAuth } from 'context/AuthContext'
+import { useRouter } from 'node_modules/next/router'
+import React, {useEffect} from 'react'
 
 const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
 
+  const {user} = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if(!user){
+      router.push('/login')
+    }
+  }, [router, user])
   return (
     children
   )
