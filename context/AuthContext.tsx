aa045b8eecd1fd462fcc058153228f10d42b429c
Collins <mutaic84@gmail.com>
2022-05-26 20:36:54 +0300

    handle setting user state

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,4 +1,30 @@
 import {createContext, useContext} from "react"
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from "react"
+import { auth } from "../config/firebase"
+
 
 const AuthContext = createContext({})
 
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthContextProvider = ({children} : {children:
+  React.ReactNode}) => {
+
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if(user){
+          setUser({
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName
+          })
+        } else {
+          setUser(null)
+        }
+      })
+      return () => unsubscribe();
+    }, [])
+    return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
+  }
