b35d2629a016d309d5d981f3a3cd5425894fe532
Collins <mutaic84@gmail.com>
2022-05-26 18:36:16 +0300

    create md5 to generate url

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import md5 from "js-md5";
+
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+const PUBLIC_KEY = ""
+const PRIVATE_KEY = ""
+
+const ts = Number(new Date());
+const hash = md5.create()
+hash.update(ts + PRIVATE_KEY + PUBLIC_KEY).digest('hex')
+
+
+export {PUBLIC_KEY, ts, hash}
